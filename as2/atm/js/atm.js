d6800805e622ff0f11828217f170e2ce368119a2
chenw25 <chenw25@gmail.com>
2015-11-11 17:16:11 -0500

    added stuff -,-

--- as2/atm/js/atm.js
+++ as2/atm/js/atm.js
@@ -25,6 +25,41 @@ function dialogAlert(title, html, action) {
     });
 };
 
+function addTransactionLog(account, oldbalance, type, amount, credit) {
+
+    var log = localStorage.getItem(account+"-log");
+
+    if(!log)
+        log = [];
+    else
+        log = JSON.parse(log);
+
+    var logitem = {
+        type:type,
+        credit:"",
+        debit:"",
+        balance:oldbalance,
+        newbalance:localStorage.getItem(account+"-balance")
+    };
+
+    var dateObj = new Date();
+    var date = dateObj.getMonth() + "/" + dateObj.getDay() + "/" + dateObj.getFullYear();
+    var time = dateObj.getHours() + ":" + (((dateObj.getMinutes()+"").length == 2 ) ? dateObj.getMinutes() : "0" + dateObj.getMinutes());
+
+    logitem.date = date;
+    logitem.time = time;
+
+    if (credit){
+        logitem.credit= "" + amount;
+    } else {
+        logitem.debit= "" + amount;
+    }
+
+    log.unshift(logitem);
+
+    localStorage.setItem(account+"-log", JSON.stringify(log));
+}
+
 function isValidAmount(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
@@ -80,7 +115,7 @@ function showtime() {
 
     var minutes = ((d.getMinutes() + "").length == 2 ) ? d.getMinutes() : "0" + d.getMinutes();
 
-    if (hours == 0)
-        hours = 12;
+    if (hours==0)
+        hours=12;
     $("#livetime").text("Current Time:  " + hours + ":" + minutes + time);
 }
