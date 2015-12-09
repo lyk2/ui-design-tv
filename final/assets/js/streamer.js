8d5ebb10a3cdbbdfea9d659ae5606c5fc6a06d83
lyk2@mcmaster.ca <lyk2@mcmaster.ca>
2015-12-09 15:16:07 -0500

    asdf

--- final/assets/js/streamer.js
+++ final/assets/js/streamer.js
@@ -4,10 +4,10 @@ function genMovieModal (title, img) {
 
     html += "<div class='container' style='width:100%'>"
     html +=     "<div class='row'>" +
-                    "<div class='col-lg-3'> " +
+                    "<div class='col-xs-3'> " +
                         '<img src="assets/img/movie/'+img+'" style="width: 90%;margin-top:22px">' +
                     "</div>" + 
-                    "<div class='col-lg-9'> " +
+                    "<div class='col-xs-9'> " +
                         "<h2>"+title+"</h2> " +
                         '<p style=" margin-top: -5px; color: #969696;margin-bottom: 20px;"><span class="glyphicon glyphicon-calendar" style=" margin-right: 5px;"></span><span style=" margin-right: 40px; ">2011</span><span class="glyphicon glyphicon-time"></span><span style=" margin-right: 40px; "> 1:47</span> <span class="glyphicon glyphicon-check"></span> <span style=" margin-right: 40px; "> PG-13</span> <span> Rating</span> <span style=" color: yellow; "> <span class="glyphicon glyphicon-star"></span> <span class="glyphicon glyphicon-star"></span> <span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span> <span class="glyphicon glyphicon-star-empty"></span></span></p>' + 
                         '<p> Director: Lorem Ipsum </p><p> Starring: Dolor Sit, Amet Consecteur, Adipiscing Elit </p>' + 
